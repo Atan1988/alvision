eabd2cc5d01060d45fb4c1e9a9fd564bf366fa47
Atan1988 <tzw0423@hotmail.com>
2019-11-17 22:27:41 -0500

    update licence and basic info

--- inst/test_codes/cv2 & azure R adaptation.R
+++ inst/test_codes/cv2 & azure R adaptation.R
@@ -9,3 +9,10 @@ azure_creds <- readr::read_rds('inst/creds/azure credential.rds')
 cropped_tm_dir <- 'inst/data/tmp_cropped/'
 parse_df <- ocr_img_wrapper(img_file = "inst\\raw_data\\ACE Contrractors Pollution_2.png", hmax = 100,
                             cropped_tm_dir = 'inst/data/tmp_cropped/', azure_creds = azure_creds)
+
+#parse_df <- readr::read_rds('inst/data/azure parsed results/ace page2.rds')
+parse_df %>%
+  purrrlyr::by_row(
+    function(row) {
+
+  })
