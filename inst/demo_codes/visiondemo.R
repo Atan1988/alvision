26f19679da5df94fef93a149896120ad45327089
Atan1988 <tzw0423@hotmail.com>
2019-11-24 08:24:16 -0500

    experienmented with magick package in R to sizing the initial image

--- inst/demo_codes/visiondemo.R
+++ inst/demo_codes/visiondemo.R
@@ -9,20 +9,26 @@ azure_creds <- readr::read_rds('inst/creds/azure credential.rds')
 cropped_tm_dir <- 'inst/data/tmp_cropped/'
 reticulate::use_condaenv('computer_vision')
 
-main_img <-  'resize-full.png'
+raw_img <- magick::image_read(img_file)
+raw_img %>% magick::image_resize('3500x3500') %>%
+  magick::image_quantize(colorspace = 'gray') %>%
+  magick::image_write('resize-full 2.png')
+main_img <-  'resize-full 2.png'
 analysis_res <- azure_vis(subscription_key = azure_creds$subscription_key,
-                          endpoint = azure_creds$endpoint, image_path = normalizePath(main_img ))
+                          endpoint = azure_creds$endpoint,
+                          image_path = normalizePath(main_img ))
 analysis_res$recognitionResult$lines -> res_lines
-match_idx <- res_lines %>% purrr::map(~pts_to_wh(.$boundingBox)) %>%
-  purrr::map(function(x) {
-    res <- bounds_list %>% purrr::map_lgl(~chk_box_in(., x, 10)) %>% which(.)
-    if (length(res) == 0) return(NA)
-    return(res)
-  })
 
 crop_out_boxes(main_img, hmax = 100) %->% c(img, img_bin, img_final_bin,
                                       contours, bounds_df, hierarchy)
 bounds_list <- bbox_df_to_c(bounds_df)
+
+match_idx <- res_lines %>% purrr::map(~pts_to_wh(.$boundingBox)) %>%
+  purrr::map_dbl(function(x) {
+    res <- bounds_list %>% purrr::map_lgl(~chk_box_in(., x, 10)) %>% which(.)
+    if (length(res) == 0) return(NA)
+    return(res)
+  })
 
 cv2$imwrite('img.png', img)
 cv2$imwrite('img bin.png', img_bin)
