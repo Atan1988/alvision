ecaf67b892c57ce61f23af018443380e7caad6c2
Atan1988 <tzw0423@hotmail.com>
2019-11-25 16:26:56 -0500

    bug fix

--- R/pre-process.R
+++ R/pre-process.R
@@ -94,8 +94,8 @@ sort_contours <- function(cnts, method="left-to-right", hmax = 100){
 #' @export
 output_cropped_img <- function(cropped_dir_path, img, idx, x, y, w, h){
   new_img <- img %>% reticulate::py_to_r() %>% .[y:(y+h), x:(x+w)]
-  cv2$imwrite(paste0(cropped_dir_path, "\\/", idx, '.png'), new_img)
-  return(paste0(cropped_dir_path, "\\/", idx, '.png'))
+  cv2$imwrite(paste0(cropped_dir_path, "/", idx, '.png'), new_img)
+  return(paste0(cropped_dir_path, "/", idx, '.png'))
 }
 
 
