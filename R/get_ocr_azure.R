3d671994caea08e129545734febd8b653fedb3d7
Allen <tzw0423@hotmail.com>
2019-11-28 13:10:26 +0000

    updated ocr_img_wrapper function

--- R/get_ocr_azure.R
+++ R/get_ocr_azure.R
@@ -174,41 +174,43 @@ vec_get_cropped_azure <- function(df) {
 #' @param hmax maximum box height to include
 #' @param cropped_tm_dir temporary dir for cropped img
 #' @param azure_creds azure credential
+#' @param box_push_to_az whether to push the individual boxes to azure
+#' @param box_highlight whether to highlight boxes, this allows the check to use results from the main push if it seems to work well
+#' @param remove_fl whether to remove files written by the box level push
 #' @export
-ocr_img_wrapper <- function(img_file, hmax = 100, cropped_tm_dir, azure_creds) {
-  crop_out_boxes(img_file, hmax) %->% c(img, img_bin, img_final_bin,
-                                                       contours, bounds_df, hierarchy)
+ocr_img_wrapper <- function(img_file, hmax = 100, cropped_tm_dir, azure_creds,
+                            box_push_to_az = F, box_highlight = F, remove_fl = F) {
+  main_img <- resize_png(img_file)
+  ##push image to azure for overall ocr
+  analysis_res <- azure_vis(subscription_key = azure_creds$subscription_key,
+                            endpoint = azure_creds$endpoint,
+                            image_path = normalizePath(main_img ))
+  analysis_res$recognitionResult$lines -> res_lines
 
-  des <- density(bounds_df$y, bw = 8, n = nrow(bounds_df), kernel = 'rectangular')
-  des_df <- tibble::tibble(x = des$x, y = des$y) %>%
-    dplyr::mutate(peak = ifelse(y > dplyr::lead(y, default = 0) & y > dplyr::lag(y, default = 0), 1, 0),
-                  trough = ifelse(y < dplyr::lead(y, default = 0) & y < dplyr::lag(y, default = 0), 1, 0)
-    ) %>%
-    dplyr::filter(peak == 1)
+  ##crop out boxes if document is a form
+  crop_out_boxes(main_img, hmax = hmax) %->% c(img, img_bin, img_final_bin,
+                                              contours, bounds_df, hierarchy)
 
-  bounds_df1 <- bounds_df %>%
-    dplyr::left_join(
-      purrr::cross_df(list(y = unique(bounds_df$y),
-                           row = ceiling(des_df$x))) %>%
-        dplyr::group_by(y) %>%
-        dplyr::mutate(dist = abs(y-row)) %>%
-        dplyr::filter(abs(dist) == min(dist)),
-      by = "y"
-    ) %>%
-    dplyr::arrange(row, x) %>%
-    dplyr::group_by(row) %>%
-    dplyr::mutate(col = seq(1, dplyr::n(), 1)) %>% dplyr::ungroup() %>%
-    dplyr::mutate(idx = seq(1, dplyr::n(), 1))
+  ##match results from main azure api push to the cropped boxes
+  bounds_df1 <- az_to_cv2_box(bounds_df, res_lines)
 
-  pb <- dplyr::progress_estimated(nrow(bounds_df1))
-  bounds_df2 <- bounds_df1 %>% #.[1:10, ] %>%
+  ##post individual boxes to azure api
+  bounds_df2 <- vec_post_cropped_azure(df = bounds_df1,
+                    cropped_tm_dir = cropped_tm_dir, img = img,
+                    azure_creds = azure_creds, push_to_az = box_push_to_az,
+                    box_highlight = box_highlight, remove_fl = remove_fl)
+
+  ##get ocr results from azure api
+  bounds_df3 <- vec_get_cropped_azure(bounds_df2)
+
+  ##creating texts from lines in the get_res column
+  parse_df1 <- bounds_df3 %>%
     purrrlyr::by_row(
       function(row) {
-        res <- get_ocr_azure(row, cropped_dir_path = cropped_tm_dir, img, azure_creds)
-        #Sys.sleep(2)
-        pb$tick()$print()
-        return(res)
-      }
-    )
-  return(bounds_df2)
+        df <- row$get_res[[1]]
+        if (nrow(df) == 0) return("")
+        df %>% dplyr::summarise(txt = paste(stringr::str_squish(txt), collapse = "  "))
+      }, .to = '.txt') %>% tidyr::unnest(cols = '.txt')
+
+  return(parse_df1)
 }
