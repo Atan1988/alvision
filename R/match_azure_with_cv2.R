7d7e488ba668cf7cc37b0d8cb3ed1e2b79700cad
Allen <tzw0423@hotmail.com>
2019-11-28 18:32:57 +0000

    changed the match code to match first, then include non-matched

--- R/match_azure_with_cv2.R
+++ R/match_azure_with_cv2.R
@@ -3,23 +3,36 @@
 #' @param res_lines lines result from the azure api reading the whole page
 #' @export
 az_to_cv2_box <- function(bounds_df, res_lines) {
-  bounds_df1 <- add_rc_bbox(bbox_df = bounds_df)
-  bounds_list <- bbox_df_to_c(bounds_df1)
+  bounds_list <- bbox_df_to_c(bounds_df)
 
   match_idx <- res_lines %>% purrr::map(~pts_to_wh(.$boundingBox)) %>%
     purrr::map_dbl(function(x) {
       res <- bounds_list %>% purrr::map_lgl(~chk_box_in(., x, 10)) %>% which(.)
       if (length(res) == 0) return(NA)
-      return(res)
+      return(res[1])
     })
 
-  bounds_df1$az <- 1:nrow(bounds_df1) %>%  purrr::map(
+  bounds_df$az <- 1:nrow(bounds_df) %>%  purrr::map(
     function(x) {
       idx <- which(match_idx == x)
       if (length(idx) == 0) return(list())
       return(res_lines[idx])
     }
   )
+
+  not_matched_idx <- which(is.na(match_idx))
+  not_matched_bounds_df <- not_matched_idx %>%
+    purrr::map_df(function(x) {
+      box_ref <- res_lines[[x]]$boundingBox %>% pts_to_wh() %>% t()
+      box_ref <- tibble::as_tibble(box_ref)
+      names(box_ref) <- c('x', 'y', 'w', 'h')
+      box_ref$az <- res_lines[x]
+      return(box_ref)
+    })
+
+  bounds_dfb <- bind_rows(bounds_df, not_matched_bounds_df)
+  bounds_df1 <- add_rc_bbox(bbox_df = bounds_dfb)
+
   return(bounds_df1)
 }
 
--- inst/demo_codes/visiondemo.R
+++ inst/demo_codes/visiondemo.R
@@ -11,7 +11,7 @@ cropped_tm_dir <- 'inst/data/tmp_cropped/'
 pdf_file <- "inst/raw_data/ACE Contrractors Pollution.pdf"
 image_files <- crt_png_from_pdf(pdf_file = pdf_file, pages = NULL, dpi = 400)
 
-img_file <- image_files[2]
+img_file <- image_files[1]
 
 # Read the image
 tictoc::tic()
@@ -26,9 +26,11 @@ analysis_res$recognitionResult$lines -> res_lines
 tictoc::toc()
 
 tictoc::tic()
-crop_out_boxes(main_img, hmax = 100) %->% c(img, img_bin, img_final_bin,
+crop_out_boxes(main_img, hmax = 200) %->% c(img, img_bin, img_final_bin,
                                       contours, bounds_df, hierarchy)
+tictoc::toc()
 
+tictoc::tic()
 bounds_df1 <- az_to_cv2_box(bounds_df, res_lines)
 
 bounds_df2 <- vec_post_cropped_azure(df = bounds_df1, cropped_tm_dir = cropped_tm_dir,
@@ -47,7 +49,20 @@ parse_df1 <- bounds_df3 %>%
     }, .to = '.txt') %>% tidyr::unnest(cols = '.txt')
 tictoc::toc()
 
+tictoc::tic()
+parse_df1b <- ocr_img_wrapper(img_file = img_file, hmax = 100,
+                        cropped_tm_dir = cropped_tm_dir, azure_creds = azure_creds,
+                        box_push_to_az = F, box_highlight = F, remove_fl = F)
+tictoc::toc()
+
 parse_df2 <- parse_df1 %>% dplyr::select(row, col, txt) %>%
+  dplyr::arrange(col, row) %>%
+  tidyr::pivot_wider(names_from = col, values_from = txt,
+                     values_fn = list(txt = ~paste(collapse = "; "))
+          )%>%
+  dplyr::arrange(row)
+
+parse_df2b <- parse_df1b %>% dplyr::select(row, col, txt) %>%
   dplyr::arrange(col, row) %>%
   tidyr::pivot_wider(names_from = col, values_from = txt) %>%
   dplyr::arrange(row)
